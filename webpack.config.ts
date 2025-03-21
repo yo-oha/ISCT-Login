94854878b41a054907cdb0e3e43dc70de7c56a47
Yoshito Ohata <79030436+yo-oha@users.noreply.github.com>
2025-03-22 01:50:08 +0900

    Support firefox

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,49 +2,68 @@ import CopyPlugin from 'copy-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { Configuration } from 'webpack'
 
-const config: Configuration = {
-  mode: (process.env.NODE_ENV ?? 'development') as Configuration['mode'],
-  entry: {
-    options: './src/options.tsx',
-    authenticate: './src/authenticate.tsx',
-    background: './src/background.ts',
-  },
-  output: {
-    path: `${__dirname}/dist`,
-    publicPath: './',
-    filename: 'js/[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        use: 'ts-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx'],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
+type BrowserTarget = 'chrome' | 'firefox'
+
+interface BuildEnv {
+  browser?: BrowserTarget
+}
+
+const config = (env: BuildEnv): Configuration => {
+  const browserTarget = env.browser ?? 'chrome'
+
+  return {
+    mode: (process.env.NODE_ENV ?? 'development') as Configuration['mode'],
+    entry: {
+      options: './src/options.tsx',
+      authenticate: './src/authenticate.tsx',
+      background: './src/background.ts',
+    },
+    output: {
+      path: `${__dirname}/dist`,
+      publicPath: './',
+      filename: 'js/[name].js',
+    },
+    module: {
+      rules: [
         {
-          from: '.',
-          to: '.',
-          context: 'public',
+          test: /.tsx?$/,
+          use: 'ts-loader',
         },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      chunks: ['options'],
-      title: 'ISCT Login Settings',
-      filename: 'options.html',
-      scriptLoading: 'blocking',
-    }),
-  ],
-  devtool: process.env.NODE_ENV === 'production' ? false : 'cheap-source-map',
-  cache: false,
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.tsx'],
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          {
+            from: '.',
+            to: '.',
+            context: 'public',
+          },
+        ],
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: '.',
+            to: '.',
+            context: `public-${browserTarget}`,
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['options'],
+        title: 'ISCT Login Settings',
+        filename: 'options.html',
+        scriptLoading: 'blocking',
+      }),
+    ],
+    devtool: process.env.NODE_ENV === 'production' ? false : 'cheap-source-map',
+    cache: false,
+  }
 }
 
 export default config
